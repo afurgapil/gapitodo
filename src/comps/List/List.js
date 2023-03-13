ed89ae3354de551c9e05eca4e65b2a5d8c5a9e0b
Gapil <gapilsoft@gmail.com>
2023-03-13 22:51:13 +0300

    v1.0

--- src/comps/List/List.js
+++ src/comps/List/List.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./list.scss";
 import "animate.css/animate.min.css";
-
+import { IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Checkbox from "@mui/material/Checkbox";
 function TodoList(props) {
   const { todos, onCompleteChange, onDelete } = props;
-
+  const label = { inputProps: { "aria-label": "Checkbox demo" } };
   if (todos.length === 0) {
     return (
       <p className="empty animate__animated animate__bounce  animate__infinite animate__slow">
@@ -18,19 +20,30 @@ function TodoList(props) {
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
-            <input
-              type="checkbox"
-              checked={todo.completed}
-              onChange={() => onCompleteChange(todo.id)}
-            />
-            <span
-              style={{
-                textDecoration: todo.completed ? "line-through" : "none",
-              }}
-            >
-              {todo.title}
-            </span>
-            <button onClick={() => onDelete(todo.id)}>Sil</button>
+            <div id="li-item">
+              <Checkbox
+                {...label}
+                checked={todo.completed}
+                color="secondary"
+                onChange={() => onCompleteChange(todo.id)}
+              />
+              <span
+                style={{
+                  textDecoration: todo.completed ? "line-through" : "none",
+                }}
+              >
+                {todo.title}
+              </span>
+            </div>
+            <div id="icon-list">
+              <IconButton
+                aria-label="delete"
+                onClick={() => onDelete(todo.id)}
+                color="error"
+              >
+                <DeleteIcon sx={{ fontSize: 40 }} />
+              </IconButton>
+            </div>
           </li>
         ))}
       </ul>
